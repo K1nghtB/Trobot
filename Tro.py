93473eb420b59de43d34760735dd55acd5ff1cf0
K1nghtB <146199422+K1nghtB@users.noreply.github.com>
2023-11-21 11:18:35 -0700

    Bug fix 1a

--- Tro.py
+++ Tro.py
@@ -4,7 +4,7 @@ name = input("Name, please? ")
 
 cmd = input("Hello " + name + ", I am TroBot. What may I do for you. Enter one of my commands or type \"Help\" to get. they are case sensitive\n")
 
-helpmenu = "\"Exe\", execute python script\n\"Quit\" Quits me"
+helpmenu = "\"Exe\", execute python script\n\"Quit\" Quits me\n \" Joke\", Makes me tell you a hacking related joke(Nyi)"
 
 if cmd == "Help":
 
@@ -13,11 +13,18 @@ if cmd == "Help":
 elif  cmd == "Exe":
 
   script = input("What script the script has to be located within the directory that contains me and be python, do not put the .py ending")
-  run(script)
-
+  try:
+    with open(script + ".py", "r") as file:
+        script_content = file.read()
+    exec(script_content)
+  except FileNotFoundError:
+        print(f"Error: File \"" + script + ".py\" not found.")
+  except Exception as e:
+        print(f"Error executing script: " + e)
 elif cmd == "Quit":
-  print(Ending)
+  print("Ending")
   quit()
+#elif cmd == "Joke"
+ 
 else:
-  print("ERROR: INVALID COMMAND\n" + helpmenu)
-  
+ print("Error: Unknown Command\n" + helpmenu)
